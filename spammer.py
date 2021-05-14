a83cdfe646591da95781c8a63f84eb743b563daa
abelareiza <abelareiza@gmail.com>
2021-05-13 22:37:24 -0500

    Added time variable and new .txt file

--- spammer.py
+++ spammer.py
@@ -4,10 +4,12 @@ import pyautogui
 import time
 
 time.sleep(5)
-f = open("shrek2.txt", "r")
-s = time.time()
+f = open("cuarteto-de-nos.txt", "r")
+t = time.time()
+s = 3.0
+
 
 for word in f:
     pyautogui.typewrite(word)
     pyautogui.press("enter")
-    time.sleep(55.0 - ((time.time() - s) % 55.0))
+    time.sleep(s - ((time.time() - t) % s))
